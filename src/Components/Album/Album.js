7faf010f0216b6f5739945255a8b4487bf866835
Luke Gebbink <lukegebbink@gmail.com>
2022-08-03 19:21:20 -0600

    Made some final changes

---
+++ src/Components/Album/Album.js
@@ -0,0 +1,127 @@
+import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import AlbumCards from "../AlbumCards/AlbumCards";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+theme.typography.h1 = {
+  [theme.breakpoints.up("xs")]: {
+    fontSize: "2rem",
+  },
+  [theme.breakpoints.up("sm")]: {
+    fontSize: "2.5em",
+  },
+  [theme.breakpoints.up("md")]: {
+    fontSize: "3rem",
+  },
+  [theme.breakpoints.up("lg")]: {
+    fontSize: "3.5rem",
+  },
+};
+
+theme.typography.h2 = {
+  [theme.breakpoints.up("xs")]: {
+    fontSize: "2rem",
+  },
+  [theme.breakpoints.up("sm")]: {
+    fontSize: "2.5em",
+  },
+  [theme.breakpoints.up("md")]: {
+    fontSize: "3rem",
+  },
+  [theme.breakpoints.up("lg")]: {
+    fontSize: "3.5rem",
+  },
+};
+
+theme.typography.h5 = {
+  [theme.breakpoints.up("xs")]: {
+    fontSize: "1rem",
+  },
+  [theme.breakpoints.up("md")]: {
+    fontSize: "1.25rem",
+  },
+  [theme.breakpoints.up("lg")]: {
+    fontSize: "1.5rem",
+  },
+};
+
+export default function Album() {
+  
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: "background.paper",
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="lg">
+            <Typography
+              component="h1"
+              variant="h1"
+              align="center"
+              color="text.primary"
+            >
+              Star Wars: The Alphabetical Wars
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="text.secondary"
+              paragraph
+            >
+              Use this guide for alphabetized marathons of
+              The Clone Wars.
+            </Typography>
+          </Container>
+        </Box>
+        <span>
+          <AlbumCards justifyContent="center"></AlbumCards>
+        </span>
+      </main>
+      {/* Footer */}
+      <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+        Made with Python, the iMDB API, React, and MUI
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          This fan-made website is not made or associated with any rightholders of Star Wars
+        </Typography>
+        <Copyright/>
+      </Box>
+      {/* End footer */}
+    </ThemeProvider>
+  );
+}
---
+++ src/Components/AlbumCards/AlbumCards.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Grid';
+import './AlbumCards.css'
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import OutputJson from '../JSON/fixeddata.json' 
+
+function AlbumCards({inputData}) {
+
+  var InputObject = JSON.stringify(OutputJson);
+  var DataJson = JSON.parse(InputObject);
+
+  return (
+    <Container>
+    {/* End hero unit */}
+
+    <Grid container spacing={0} gap={4} margin='auto' justifyContent='center'>
+     {DataJson.map((item, index) => (
+        <Card
+            sx={{ maxHeight: '45%', maxWidth: 325, display: 'flex', flexDirection: 'column'}}
+            key={index}
+            variant="outlined" 
+          >
+            <CardMedia
+              component="img"
+              className='episodeImage'
+              src= {item.imageURL}
+              alt="Image from Star Wars: The Clone Wars"
+            />
+            <CardContent sx={{ flexGrow: 1 }}>
+              {/* EpisodeTitle */}
+              <Typography sx={{ fontWeight: 'bold' }} gutterBottom variant="h5" component="h2" className='episodeTitle'>
+                  {item.episodeTitle}
+              </Typography>
+              <Typography className='seasonAndEpisodeIndicator'>
+                {"Season " + item.seasonIndex + ", Episode " + item.episodeIndex}
+              </Typography>
+              <Typography className='episodePlot' variant='h7'>
+                  {/* Plot Title */}
+                  {item.plot}
+              </Typography>
+            </CardContent>
+          </Card>  
+      ))}  
+    </Grid>
+  </Container>
+  )
+}
+
+export default AlbumCards
